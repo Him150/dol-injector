cf18f56f2c546e64319bfb0da29807aa96a6f6aa
LukasHim <52617815+LukasHim@users.noreply.github.com>
2026-02-21 13:36:59 +0800

    添加云备份功能和更新工作流配置

---
+++ README.md
@@ -0,0 +1,6 @@
+# Degrees of Lewdity 功能注入
+
+原仓库 [Eltirosto/Degrees-of-Lewdity-Chinese-Localization](https://github.com/Eltirosto/Degrees-of-Lewdity-Chinese-Localization)  
+自动添加功能分支
+
+> 添加 云备份
---
+++ src/repalcer.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+
+console.log(process.env.WORK_FOLDER_PATH, process.env.BUILD_FOLDER_PATH);
+
+const file = fs.readFileSync(path.resolve(process.env.WORK_FOLDER_PATH ?? '', 'dist/injector.js')).toString();
+
+const cspMetaTest = /<meta[^\<]*?["']Content-Security-Policy["'][^\>]*?>/is;
+const gameVersionTest = '#gameVersionDisplay {';
+const insertTest = '</head>';
+
+(async () => {
+  replace('index.html');
+  replace('Degrees of Lewdity VERSION.html.mod.html');
+  replace('Degrees of Lewdity VERSION.html.mod-polyfill.html');
+})();
+function replace(filePath) {
+  let filePathSolved = path.resolve(process.env.BUILD_FOLDER_PATH ?? '', filePath);
+  if (fs.existsSync(filePathSolved)) {
+    let text = fs.readFileSync(filePathSolved).toString();
+    if (cspMetaTest.test(text)) {
+      console.log('replace cspMetaTest');
+      text = text.replace(cspMetaTest, '');
+    }
+    if (new RegExp(gameVersionTest).test(text)) {
+      console.log('replace gameVersionTest');
+      text = text.replace(new RegExp(gameVersionTest), gameVersionTest + 'display:none;');
+    }
+    if (new RegExp(insertTest).test(text)) {
+      console.log('replace insertTest');
+      text = text.replace(new RegExp(insertTest), `<script>${file}</script>\n${insertTest}`);
+    }
+    fs.writeFileSync(filePathSolved, text);
+    return;
+    fs.writeFileSync('modified' + filePath + '', text);
+  } else {
+    console.log("File didn't exist:", filePath);
+  }
+}
